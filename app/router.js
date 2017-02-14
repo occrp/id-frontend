8ea9b42179ec9c06b879147fa3e2fe64979ebb27
GreenDude <schipor@gmail.com>
2017-02-14 19:19:57 +0200

    Change the URL paths a bit.

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('browse');
+  this.route('browse', { path: '/view' });
   this.route('new');
   this.route('view', { path: '/view/:ticket_id' });
 });
