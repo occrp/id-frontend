f68b9001e816092f09090d62d4bb5139ddd36d7f
GreenDude <schipor@gmail.com>
2017-03-20 21:23:48 +0200

    Make the wrapper link change routes; Update /view.

--- app/pods/view/route.js
+++ app/pods/view/route.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('ticket', params.ticket_id);
+    return this.get('store').findRecord('ticket', params.ticket_id, {
+      include: 'author,assignee'
+    });
   }
 });
