d7c22e6b306a84afa721a0d097b530cb85816276
Stas SUȘCOV <stas@nerd.ro>
2017-04-26 16:45:14 +0300

    Disable fingerprinting.
    The package will be distributed via npm/bower.
    Fingerprints make it hard to integrate.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
 
+    fingerprint: {
+      enabled: false
+    },
+
     outputPaths: {
       app: {
         css: {
