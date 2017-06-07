cd327bac72b23d213099386eba54901b517f8ad4
GreenDude <schipor@gmail.com>
2017-06-07 20:58:02 +0300

    Update mirage to 3.3. Nuke mirage models 😏

--- mirage/models/ticket.js
+++
@@ -1,7 +0,0 @@
-import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
-
-export default Model.extend({
-  author: belongsTo('user'),
-  assignee: belongsTo('user'),
-  activities: hasMany(),
-});
