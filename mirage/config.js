698d364e1a41fa4628bf687d9ca26e6e4f40ae3e
GreenDude <schipor@gmail.com>
2017-07-27 18:03:12 +0300

    Update active class on tabs re: filter changes.

--- mirage/config.js
+++ mirage/config.js
@@ -20,7 +20,7 @@ export default function() {
 
 
   this.get('/tickets', (schema, request) => {
-    let status = request.queryParams['filter[status__in]'];
+    let status = request.queryParams['filter[status__in]'].split(',');
 
     let collection = schema.tickets.where(function(ticket) {
       return status.includes(ticket.status);
