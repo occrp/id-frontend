912c4bef9ab049b76d5baa64dd3a75495c0b55dd
GreenDude <schipor@gmail.com>
2017-08-03 12:48:30 +0300

    Underscore endpoint sort values.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,22 @@
 import DS from 'ember-data';
+import Ember from "ember";
+
+const { underscore } = Ember.String;
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api/v3'
+  namespace: 'api/v3',
+  
+  // underscore sort values, keep the order dash prefix
+  urlForQuery (query) {
+    if (query.sort) {
+      const orderDashIndex = query.sort.indexOf('-');
+      const splitIndex = orderDashIndex === 0 ? 1 : 0;
+      const prefix = orderDashIndex === 0 ? '-' : '';
+      const processed = underscore(query.sort.substring(splitIndex));
+
+      query.sort = prefix + processed;
+    }
+
+    return this._super(...arguments);
+  }
 });
