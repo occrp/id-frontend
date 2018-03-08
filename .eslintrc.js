096666a845cfc3c1a9c2277211127c8b04fcde1c
sungraze <schipor@gmail.com>
2018-03-08 09:59:17 +0200

    Upgrade stack to 2.18; also add package-lock; * running on node 9.8.0 / npm 5.6.0 * also bump ember-can to 0.8.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,47 @@ module.exports = {
       experimentalObjectRestSpread: true
     }
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    },
+
+    // test files
+    {
+      globals: {
+        server: true,
+      },
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
