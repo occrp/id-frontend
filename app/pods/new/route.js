3813df690808298643b67e2429a4f1ceb674599c
Stas SUȘCOV <stas@nerd.ro>
2020-03-19 19:14:32 +0000

    Redirect to user screen if bio is missing.

--- app/pods/new/route.js
+++ app/pods/new/route.js
@@ -1,6 +1,15 @@
 import Route from '@ember/routing/route';
+import { isBlank } from '@ember/utils';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
+  redirect() {
+    if (isBlank(this.get('session.currentUser.bio'))) {
+      this.transitionTo('user');
+    }
+  },
   model() {
     return this.get('store').createRecord('ticket');
   },
