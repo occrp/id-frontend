11b4cb8b3b092b3211cf540648e7584ff5c11a96
Stas SUȘCOV <stas@nerd.ro>
2020-03-25 12:14:01 +0000

    Remove some CSS.

--- app/pods/components/ui-dropzone/component.js
+++
@@ -1,5 +0,0 @@
-import FileDropzone from 'ember-file-upload/components/file-dropzone/component';
-
-export default FileDropzone.extend({
-  classNameBindings: ['active:is-active', 'valid:is-valid:is-invalid'],
-});
