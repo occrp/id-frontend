d4b1dc07bd53f1f12f7c3883f920bab88c5516b9
Stas SUȘCOV <stas@nerd.ro>
2020-03-25 19:06:28 +0000

    Updated user screen and added bio to user card.

--- app/pods/expenses/route.js
+++ app/pods/expenses/route.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    let result = this._super(...arguments);
+
+    if (!this.get('can').can('manage tickets')) {
+      return this.transitionTo('index');
+    }
+
+    return result;
+  },
+
   model(params) {
     return this.get('store').findRecord('ticket', params.ticket_id, {
       include: 'requester,responders,responders.user,subscribers,subscribers.user,attachments,attachments.user',
--- app/pods/new/route.js
+++ app/pods/new/route.js
@@ -5,14 +5,20 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   session: service(),
 
-  redirect() {
+  beforeModel() {
+    let result = this._super(...arguments);
+
     if (isBlank(this.get('session.currentUser.bio'))) {
       this.transitionTo('user');
     }
+
+    return result;
   },
+
   model() {
     return this.get('store').createRecord('ticket');
   },
+
   deactivate() {
     this.currentModel.rollbackAttributes();
   }
