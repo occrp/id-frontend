bc0a3d8486a8b7b2e994e9a184a3a705ea8ef983
Stas SUȘCOV <stas@nerd.ro>
2020-04-26 23:05:09 +0100

    Added countries card.

---
+++ app/pods/view/ticket/countries-card/component.js
@@ -0,0 +1,40 @@
+import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+import countries from 'i18n-iso-countries';
+
+export default Component.extend({
+  countries: countries.getNames('en'),
+  flashMessages: service(),
+
+  selectedCountry: null,
+
+  actions: {
+    addCountry() {
+      const model = this.get('model');
+      let countries = model.get('countries') || [];
+
+      countries.push(this.get('selectedCountry'));
+      countries = countries.uniq();
+
+      model.set('countries', countries);
+
+      // TODO: Add validations
+      if (model.get('isValid')) {
+        model.save().then(()=> {
+          this.set('selectedCountry', null);
+        });
+      }
+    },
+
+    removeCountry(code) {
+      const model = this.get('model');
+
+      model.get('countries').removeObject(code);
+
+      // TODO: Add validations
+      if (model.get('isValid')) {
+        model.save();
+      }
+    }
+  }
+});
